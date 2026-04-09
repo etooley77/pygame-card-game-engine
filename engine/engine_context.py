f88eecf65c9d81473feca14e796877c4354828cc
etooley77 <tooleye77@gmail.com>
2026-04-09 18:15:15 -0400

    create engine context to pass down to games

---
+++ engine/engine_context.py
@@ -0,0 +1,7 @@
+
+
+class EngineContext:
+	def __init__(self, asset_manager, renderer, input_system):
+		self.asset_manager = asset_manager
+		self.renderer = renderer
+		self.input_system = input_system
