3403f8f1331edfd9ed25cabf4150e7aff3c30df0
etooley77 <tooleye77@gmail.com>
2026-04-08 22:18:47 -0400

    create test Game class for engine development

---
+++ game/game.py
@@ -0,0 +1,12 @@
+
+
+class Game:
+	def __init__(self):
+		self.window_size = (800, 600)
+		self.title = "Test Game"
+
+		def update(self, dt, input_system):
+			pass
+
+		def render(self, renderer):
+			pass
