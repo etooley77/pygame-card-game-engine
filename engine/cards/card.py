b8e8f488404be0ceb05e90e857dc8bfd20e47df1
etooley77 <tooleye77@gmail.com>
2026-04-09 23:15:59 -0400

    implement card scaling upon hover

---
+++ engine/cards/card.py
@@ -0,0 +1,52 @@
+from pygame.transform import scale_by
+
+from engine.cards.behaviors.draggable import Draggable
+
+class Card(Draggable):
+	def __init__(self, image, pos):
+		super().__init__()
+		self.image = image
+		self.scale_image = False
+		self.scale_factor = 1.1 # Must be greater than one
+
+		self.pos = pos
+		self.rect = self.image.get_rect(topleft=pos)
+
+	def update(self, input_context):
+		mouse_pos = input_context.mouse_pos
+		hovered = self.rect.collidepoint(mouse_pos)
+
+		# Use scaled image
+		if hovered:
+			self.scale_image = True
+
+		# Start dragging
+		if hovered and input_context.mouse_pressed:
+			self.start_drag(mouse_pos, self.pos)
+
+		# Change position via dragging
+		if self.dragging and input_context.mouse_down:
+			self.pos = self.drag(mouse_pos)
+
+		# Stop dragging
+		if self.dragging and input_context.mouse_released:
+			self.stop_drag()
+
+		# Stop scaling the image
+		if not hovered:
+			self.scale_image = False
+
+		self.rect.topleft = self.pos
+
+	def render(self, surface):
+		if self.scale_image:
+			surface.blit(scale_by(self.image, self.scale_factor), self.calculate_scaled_pos())
+		else:
+			surface.blit(self.image, self.pos)
+
+	def calculate_scaled_pos(self):
+		w = self.image.get_width()
+		h = self.image.get_height()
+		s = self.scale_factor
+	
+		return (self.pos[0] - 0.5 * w * (s - 1), self.pos[1] - 0.5 * h * (s - 1))
