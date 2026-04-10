de420865e5d2173fb05b37b2319465da4f49d3a0
etooley77 <tooleye77@gmail.com>
2026-04-09 23:19:15 -0400

    refactor input system with an input context pipeline

--- engine/input/input_system.py
+++ engine/input/input_system.py
@@ -1,21 +1,34 @@
 import pygame
 from sys import exit
 
+from engine.input.input_context import InputContext
+
 class InputSystem:
 	def __init__(self):
 		self.input_queue = []
-		print("InputSystem initialized!")
+
+		self.input_context = InputContext()
 
 	def monitor(self):
-		self.input_queue.clear()
+		self.input_context.clear()
 
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				pygame.quit()
 				exit()
+
 			if event.type == pygame.KEYDOWN:
 				self.input_queue.append(event)
-			if event.type == (pygame.MOUSEBUTTONDOWN or pygame.MOUSEBUTTONUP):
-				self.input_queue.append(event)
+			
+			if event.type == pygame.MOUSEBUTTONDOWN:
+				self.input_context.mouse_pressed = True
+				self.input_context.mouse_down = True
 
-		return self.input_queue
+			if event.type == pygame.MOUSEBUTTONUP:
+				self.input_context.mouse_down = False
+				self.input_context.mouse_released = True
+
+		self.input_context.events = self.input_queue
+		self.input_context.mouse_pos = pygame.mouse.get_pos()
+
+		return self.input_context
