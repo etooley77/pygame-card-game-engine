3e64fcf46b4de7d7416cbe6d7fc62deca4afd3f4
etooley77 <tooleye77@gmail.com>
2026-04-14 21:47:58 -0400

    refactor EventHandler for more flexibility

--- engine/input/event_handler.py
+++ engine/input/event_handler.py
@@ -1,4 +1,5 @@
 import json
+import pygame
 
 class EventHandler:
 	keybinds = None
@@ -12,9 +13,42 @@ class EventHandler:
 		actions = []
 
 		for event in events:
-			try:
-				actions.append(EventHandler.keybinds[f"{event.key}"])
-			except:
-				continue
+			match event.type:
+				case pygame.MOUSEBUTTONDOWN:
+					actions.append(EventHandler.decode_mouse_down_event(event.button))
+
+				case pygame.MOUSEBUTTONUP:
+					actions.append(EventHandler.decode_mouse_up_event(event.button))
+
+				case pygame.KEYDOWN:
+					try:
+						actions.append(EventHandler.keybinds[f"{event.key}"])
+					except:
+						continue
+
+				case _:
+					continue
 
 		return actions
+	
+	@staticmethod
+	def decode_mouse_down_event(button):
+		match button:
+			case 1: return "mouse_left_down"
+			case 2: return "mouse_middle_down"
+			case 3: return "mouse_right_down"
+
+			case _: pass
+
+	@staticmethod
+	def decode_mouse_up_event(button):
+		match button:
+			case 1: return "mouse_left_up"
+			case 2: return "mouse_middle_up"
+			case 3: return "mouse_right_up"
+
+			case _: pass
+
+	@staticmethod
+	def decode_keypress_event(dict):
+		pass
